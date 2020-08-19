a1f7871cfb102f5d65da342918b13546d331691d
19449703 <19449703@163.com>
2020-08-20 01:49:41 +0800

    决策树，未完

--- 4. 决策树/DecisionTree.py
+++ 4. 决策树/DecisionTree.py
@@ -2,14 +2,13 @@ import numpy as np
 import pandas as pd
 import math
 
-class DesitionTree:
+class DecisionTree:
     # 定义节点类 二叉树
     class Node:
-        def __init__(self, parent = None, label = None, feature_name = None, feature = None):
+        def __init__(self, parent = None, label = None, feature_name = None):
             self.parent = parent
             self.label = label
             self.feature_name = feature_name
-            self.feature = feature
             self.child = {}
 
             self.result = {
@@ -24,11 +23,12 @@ class DesitionTree:
         def add_node(self, val, node):
             self.child[val] = node
 
-        def predict(self, features):
-            if self.parent == None:
+        def predict(self, data):
+            if len(self.child) == 0:
                 return self.label
 
-            return self.child[features[self.feature]].predict(features)
+            key = data.loc[self.feature_name]
+            return self.child[key].predict(data)
 
 
     def __init__(self, type = 'id3', epsilon=0.1):
@@ -66,7 +66,7 @@ class DesitionTree:
         # 特征A的每个类别对应的样本集
         feature_split_data = {c : [data for data in datasets if data[feature_col] == c] for c in feature_set}
         # 经验条件熵
-        cond_ent = sum([len(datas) / data_len * DesitionTree.entropy(datas) for c, datas in feature_split_data.items()])
+        cond_ent = sum([len(datas) / data_len * DecisionTree.entropy(datas) for c, datas in feature_split_data.items()])
         
         return cond_ent
 
@@ -81,7 +81,7 @@ class DesitionTree:
         # 特征A的类别集合
         feature_set = set(feature_data)
         # 特征A的每个类别的样本数占总全样本数之比
-        feature_count_radio = [feature_data.Count(value) / data_len for value in feature_set]
+        feature_count_radio = [feature_data.count(value) / data_len for value in feature_set]
         
         ent = -sum([ratio * math.log(ratio, 2) for ratio in feature_count_radio])
         return ent
@@ -93,18 +93,18 @@ class DesitionTree:
         # 样本维度
         feature_count = len(datasets[0])
         # 经验熵
-        ent = DesitionTree.entropy(datasets)
+        ent = DecisionTree.entropy(datasets)
         # 准则
         rules = []
 
         for feature_col in range(feature_count - 1):
-            cond_ent = DesitionTree.cond_entropy(datasets, feature_col)
+            cond_ent = DecisionTree.cond_entropy(datasets, feature_col)
 
             if type == 'id3':
                 info_gain = ent - cond_ent  # 信息增益
                 rules.append((feature_col, info_gain))
             elif type == 'c4.5':
-                feature_ent = DesitionTree.cond_entropy(datasets, feature_col)
+                feature_ent = DecisionTree.feature_entropy(datasets, feature_col)
                 info_gain_ratio = (ent - cond_ent) / feature_ent # 信息增益比
                 rules.append((feature_col, info_gain_ratio))
 
@@ -116,7 +116,7 @@ class DesitionTree:
 
     def train(self, train_data, parent_node = None):
         """
-        input:数据集D(DataFrame格式，label位于最后一列)，特征集A，阈值eta
+        input:数据集D(DataFrame格式，label位于最后一列)，特征集A
         output:决策树T
         """
 
@@ -124,41 +124,56 @@ class DesitionTree:
         
         # 1.若D中实例属于同一类Ck，则T为单节点树，并将类Ck作为结点的类标记，返回T
         if len(y_train.value_counts()) == 1:
-            return DesitionTree.Node(parent = parent_node, label = y_train.iloc[0])
+            return DecisionTree.Node(parent = parent_node, label = y_train.iloc[0])
 
         # 2.若特征集A为空，则T为单节点树，将D中实例树最大的类Ck作为该节点的类标记，返回T
         if len(features) == 0:
             max_label = y_train.value_counts(ascending = False).iloc[0]
-            return DesitionTree.Node(parent = parent_node, label = max_label)
+            return DecisionTree.Node(parent = parent_node, label = max_label)
 
         # 3.计算特征集A中各特征对D的信息增益(或信息增益比)，选择最大的特征Ag
-        max_feature_col, max_feature_rule_value = DesitionTree.choose_best_feature(np.array(train_data), self.type)
+        max_feature_col, max_feature_rule_value = DecisionTree.choose_best_feature(np.array(train_data), self.type)
         max_feature_name = features[max_feature_col]
 
         # 4.如果Ag的信息增益(或信息增益比)小于阈值eta，则置T为单节点树，并将D中实例数最大的类Ck作为该节点的类标记，返回T
         if max_feature_rule_value < self.eta:
             max_label = y_train.value_counts(ascending = False).iloc[0]
-            return DesitionTree.Node(parent = parent_node, label = max_label)
+            return DecisionTree.Node(parent = parent_node, label = max_label)
 
         # 5.否则，对Ag的每一可能值ai，依Ag=ai将D分割为若干非空子集Di，将Di中实例数最大的类作为标记，构建子结点，由结点及其子结点构成树T，返回T；
-        node = DesitionTree.Node(parent = parent_node, feature_name = max_feature_name, feature = max_feature_col)
+        node = DecisionTree.Node(parent = parent_node, feature_name = max_feature_name)
         
         max_feature_value_list = train_data[max_feature_name].value_counts().index
         for v in max_feature_value_list:
             sub_train_df = train_data.loc[train_data[max_feature_name] == v].drop([max_feature_name], axis = 1)
-            sub_node = self.train(sub_train_df)
+            sub_node = self.train(sub_train_df, parent_node)
             node.add_node(v, sub_node)
 
         return node
 
 
     def fit(self, train_data):
+        """
+        input:数据集D(DataFrame格式，label位于最后一列)
+        output:决策树T
+        """
         self.tree = self.train(train_data)
         return self.tree
 
 
-    def predict(self, X_test):
-        return self.tree.predict(X_test)
+    def predict(self, test_data):
+        """
+        input:测试数据集(DataFrame格式，label位于最后一列)
+        output:预测结果
+        """
+
+        x_test, y_test = test_data.iloc[:, :-1], test_data.iloc[:, -1]
+        result = []
+
+        for index, row in x_test.iterrows():
+            result.append(self.tree.predict(row))
+
+        return result
 
 
 # test
@@ -189,12 +204,40 @@ def create_data():
 datasets, labels = create_data()
 data_df = pd.DataFrame(datasets, columns=labels)
 
-# max_feature_col, max_feature_rule_value = DesitionTree.choose_best_feature(datasets, 'id3')
-# print(max_feature_col)
-# print(max_feature_rule_value)
-
-dt = DesitionTree()
+dt = DecisionTree()
 tree = dt.fit(data_df)
 print(tree)
-pred = dt.predict(['老年', '否', '否', '一般'])
+pred = dt.predict(data_df)
 print(pred)
+print(data_df.iloc[:, -1].tolist())
+
+
+# from sklearn.datasets import load_iris
+# from sklearn.model_selection import train_test_split
+
+# iris = load_iris()
+# df = pd.DataFrame(iris.data, columns=iris.feature_names)
+# df['label'] = iris.target
+# df.columns = [
+#     'sepal length', 'sepal width', 'petal length', 'petal width', 'label'
+# ]
+
+# df_train, df_test = train_test_split(df, test_size=0.3, random_state=999)
+
+# data = np.array(df)
+# X, y = data[:, :-1], data[:, -1]
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=999)
+
+# # scikit-learn实例
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.tree import export_graphviz
+
+# clf = DecisionTreeClassifier()
+# print(clf.fit(X_train, y_train))
+# print(clf.score(X_test, y_test))
+
+# print('---------------- 漂亮的分割线 ----------------')
+
+# dt = DecisionTree()
+# tree = dt.fit(df_train)
+# dt.predict(df_test)
